debb1691b6bbde44b3fbafaec78aba19cdf94d95
1F10250055 <s1f102500551@iniad.org>
2026-07-01 19:53:45 +0900

    修正1

--- todo/tests.py
+++ todo/tests.py
@@ -84,7 +84,7 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.context['tasks'][1], task2)
 
     def test_detail_get_success(self):
-        task = Task(title='task1', due_at=taimezone.make_aware(datetime(2024, 7, 1)))
+        task = Task(title='task1', due_at=timezone.make_aware(datetime(2024, 7, 1)))
         task.save()
         cilent = Client()
         response = client.get('/{}/'.format(task.pk))
